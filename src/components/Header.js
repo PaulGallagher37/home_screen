5f2a5dc0f3f0465e97d6f95f1b77a46f22cfa79c
Paul G <paulgallagher37@yahoo.co.uk>
2025-06-28 10:48:18 +0100

    adding Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Log In</li>
+                <li>Lists</li>
+            </ul>
+        </div>
+    )
+};
+
+export default Header;
