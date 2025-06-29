d24e86e6fd44b32aae696f7c888499eecbeca0c3
Paul G <paulgallagher37@yahoo.co.uk>
2025-06-29 12:06:08 +0100

    starting header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,28 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            <header class="p-3 text-bg-dark"> 
-                <div class="container"> 
-                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"> 
-                        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"></a>
-                        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                            <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li> 
-                            <li><a href="#" class="nav-link px-2 text-white">Features</a></li> 
-                            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li> 
-                            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li> 
-                            <li><a href="#" class="nav-link px-2 text-white">About</a></li> 
-                        </ul> 
-                        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search"> 
-                            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"></input>
-                        </form> 
-                        <div class="text-end"> 
-                            <button type="button" class="btn btn-outline-light me-2">Login</button> 
-                            <button type="button" class="btn btn-warning">Sign-up</button> 
-                        </div> 
-                    </div> 
-                </div> 
-            </header>
+        <div className="header">
+
+            <div className="header-one">
+                <ul  className="header-ul">
+                    <li><a href="#" className="header-a">Home</a></li> 
+                    <li><a href="#" className="header-a">Features</a></li> 
+                    <li><a href="#" className="header-a">Pricing</a></li> 
+                    <li><a href="#" className="header-a">FAQs</a></li> 
+                    <li><a href="#" className="header-a">About</a></li> 
+                </ul> 
+            </div>
+
+            <div className="header-two">
+                <form> 
+                    <input type="search" placeholder=" Search..." className="header-form"></input>
+                </form> 
+                <div className="header-button-container"> 
+                    <button type="button" className="header-login-button">Login</button> 
+                    <button type="button" className="header-signup-button">Sign-up</button> 
+                </div>   
+            </div>   
+
         </div>
     )
 };
