ee0d0555e9c129919e92ef61190dedfc4751c2ed
Paul G <paulgallagher37@yahoo.co.uk>
2025-07-01 05:53:21 +0100

    adding typing text

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
 import main_image from "../images/retrogaming.png"
 
+
 function Main(){
+
+    const text = "Home Screen";
+    let index = 0;
+
+    function typewriter() {
+        if (index < text.length) {
+            document.getElementById("typed-text").textContent += text.charAt(index);
+            index++;
+            setTimeout(typewriter, 500)
+        }
+    }
+
+    useEffect(() => {
+        typewriter();
+    },[])
+
     return (
         <div className="main-container">
             <div className="main-title-container">
-                <h1 className="main-h1">Home Screen</h1>
+                <h1 className="main-h1" id="typed-text"></h1>
                 <p className="main-p1">Discover, track, analyze your games</p>
             </div> 
             <img className="main-img" src={main_image} width="650"></img>
