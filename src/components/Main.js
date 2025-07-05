48932f0eb4107060ea7f1a4cf4e75017ffb2b9d3
Paul G <paulgallagher37@yahoo.co.uk>
2025-07-05 20:29:01 +0100

    renaming css selectors

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,30 +5,38 @@ import controllers from "../images/controllers.jpg"
 function Main(){
 
     return (
-        <div className="main-container">
+
+        <div className="main-page-container">
             <div className="main-title-container">
                 <h1 className="main-h1" id="typed-text">Home Screen</h1>
-                <h2 className="main-p1">Discover, track, analyze your games</h2>
+                <h2 className="main-p-1">Discover, track, analyze your games</h2>
             </div> 
 
 
-            <div className="main-h3-p-img">
-                <div className="main-h3-p">
-                    <h3 className="main-h3">What is Home Screen?</h3>
-                    <p className="main-p">Think of Home Screen as a 'To-Do List' for your backlog. 
+            <div className="section1-container1">
+                <div className="section1-h3-p">
+                    <h3 className="section-h3">What is Home Screen?</h3>
+                    <p className="section1-p1">Think of Home Screen as a 'To-Do List' for your backlog. 
                         A place to list and keep track of all the games you still need 
                         to start, play, and finish!
+                        Sed facilisis sapien orci, id mattis erat efficitur nec. 
+                        Sed facilisis sapien orci, id mattis erat efficitur nec. 
                     </p>
                 </div>
                 <img className="main-img" src={main_image} width="650"></img>
             </div>
 
 
-            <div className="main-h3-p-img">
+            <div className="section2-container1">
                 <img className="main-img" src={controllers} width="600" height="350"></img>
-                <div className="main-h3-p">
-                    <h3 className="main-h3">Lists, lists...and more lists!</h3>
-                    <p className="main-p">
+                <div className="section2-h3-p">
+                    <h3 className="section-h3">Lists, lists...and more lists!</h3>
+                    <p className="section2-p1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        In venenatis, est eget suscipit rutrum, ligula metus 
+                        suscipit ipsum, ac convallis mi arcu eleifend ligula. 
+                        Sed facilisis sapien orci, id mattis erat efficitur nec. 
+                        Sed facilisis sapien orci, id mattis erat efficitur nec. 
                     </p>
                 </div>
             </div>
