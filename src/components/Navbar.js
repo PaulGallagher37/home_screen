30139a78728ac318122ce9e3d002782f147264d4
Paul G <paulgallagher37@yahoo.co.uk>
2025-07-11 20:00:52 +0100

    working on navbar Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="header-page-container">
+        <div className="navbar-page-container">
 
-            <div className="header-one">
-                <ul  className="header-ul">
-                    <li><Link to="/" className="header-li">Home</Link></li>
-                    <li><Link to="/list" className="header-li">Lists</Link></li>
-                    <li><Link to="faq" className="header-li">FAQs</Link></li>
+            <div className="navbar-one">
+                <ul  className="navbar-ul">
+                    <li><Link to="/" className={"navbar-li"}>Home</Link></li>
+                    <li><Link to="/list" className={"navbar-li"}>Lists</Link></li>
+                    <li><Link to="faq" className={"navbar-li"}>FAQs</Link></li>
                 </ul> 
             </div>
 
-            <div className="header-two">
+            <div className="navbar-two">
                 <div className="header-button-container"> 
                     <button type="button" className="header-login-button">Login</button> 
                     <button type="button" className="header-signup-button">Sign-up</button> 
