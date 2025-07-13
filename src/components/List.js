0a1ceba627a14281fe84c2f772d0e113e0302b62
Paul G <paulgallagher37@yahoo.co.uk>
2025-07-13 20:20:58 +0100

    working on List functionality

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,10 @@
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 function List() {
+
+    const [ gamesToPlay, setGamesToPlay ] = useState("");
+    const [ gamesBeat, setGamesBeat ] = useState("");
 
      function handleSubmit(event) {
             
@@ -11,31 +15,34 @@ function List() {
 
         <div>
             <Navbar />
-            <div className="main-list-container"> 
+            <form>
+                <div className="main-list-container"> 
 
-                <div className="list-container">
-                    <h2>Games to Play</h2>
-                </div>
-
-                <div className="list-addgame-container">
-                    <h1 className="list-addgame-h1">Add Game</h1>
-                    <input 
-                        className="list-addgame-input" 
-                        placeholder="  Add game..."
-                        onSubmit={handleSubmit}
-                    >
-                    </input>
-                    <div className="list-button-container">
-                        <button className="list-button">Game to Play</button>
-                        <button className="list-button">Game Beaten</button>
+                    
+                    <div className="list-container">
+                        <h2>Games to Play</h2>
                     </div>
-                </div>
 
-                <div className="list-container">
-                    <h2>Games Beat</h2>
-                </div>
+                    <div className="list-addgame-container">
+                        <h1 className="list-addgame-h1">Add Game</h1>
+                        <input 
+                            className="list-addgame-input" 
+                            type="text"
+                            placeholder="  Add game..."
+                        >
+                        </input>
+                        <div className="list-button-container">
+                            <button className="list-button" onClick={handleSubmit}>Game to Play</button>
+                            <button className="list-button" onClick={handleSubmit}>Game Beaten</button>
+                        </div>
+                    </div>
 
-            </div>
+                    <div className="list-container">
+                        <h2>Games Beat</h2>
+                    </div>
+
+                </div>
+            </form>
         </div>
         
     )
